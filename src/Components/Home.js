26249c1df7f8106a7d106e3f57c8fdfc61244eeb
alantjose <f20180187@goa.bits-pilani.ac.in>
2020-08-06 21:57:24 +0530

    bg fixes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,16 +3,19 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl text-white uppercase my-32 antialiased font-extrabold">
-        Aradhana Fellowship
-        <br />
-        Video Chat App
-      </h1>
-      <div
-        className={`text-4xl bg-orange-200 inline uppercase px-10 py-5 rounded-lg 
+      <div className="h-screen   z-10 body">
+        <h1 className="text-6xl text-white uppercase py-32 antialiased font-extrabold">
+          Aradhana Fellowship
+          <br />
+          Video Chat App
+        </h1>
+        <a
+          href="https://jitsi.org/"
+          className={`text-4xl bg-orange-200 inline uppercase px-10 py-5 rounded-lg cursor-pointer 
         font-bold shadow-xl  mt-20`}
-      >
-        Start Video Call
+        >
+          Start Video Call
+        </a>
       </div>
     </div>
   );
