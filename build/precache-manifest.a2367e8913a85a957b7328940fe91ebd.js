f6d80575c563abc3ebfa1f0b6d21f957b9d08093
alantjose <f20180187@goa.bits-pilani.ac.in>
2020-08-06 22:53:51 +0530

    build

---
+++ build/precache-manifest.a2367e8913a85a957b7328940fe91ebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6aa87f35fda849a34782b3abcf727d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001152f8b1da9de72ba9",
+    "url": "/static/css/main.b383a77b.chunk.css"
+  },
+  {
+    "revision": "ec5bd58be9a82bb8401e",
+    "url": "/static/js/2.ab4d9fdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab4d9fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001152f8b1da9de72ba9",
+    "url": "/static/js/main.a4e68696.chunk.js"
+  },
+  {
+    "revision": "5345a117a2b0dde5a79c",
+    "url": "/static/js/runtime-main.871780f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
